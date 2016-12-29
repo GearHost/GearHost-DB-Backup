1a7dc5ea43f03b5d6ef2da4787e88b06fa998893
Serhiy Bardakov <serhiy.bardakov@gearhost.com>
2016-12-29 07:31:29 -0700

    Infinite donwload timeout

--- GearHostWebClient.cs
+++ GearHostWebClient.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Net;
+using System.Threading;
 
 namespace GearHost.Database.Backup
 {
     public class GearHostWebClient : WebClient
     {
-        private const int REQUEST_TIMEOUT_MINUTES = 60;
-
         protected override WebRequest GetWebRequest(Uri uri)
         {
             WebRequest w = base.GetWebRequest(uri);
-            w.Timeout = (int)TimeSpan.FromMinutes(REQUEST_TIMEOUT_MINUTES).TotalMilliseconds;
+            w.Timeout = Timeout.Infinite;
             return w;
         }
     }
